5854c7c66e0d25698807740315d60050baca5d14
Rune Nielsen <rune2799@elev.techcollege.dk>
2022-10-13 13:33:36 +0200

    Add project files.

---
+++ Entity framework opgave/Migrations/20221013112845_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entity_framework_opgave.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: false),
+                    Complete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TaskID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Posts_TaskID",
+                        column: x => x.TaskID,
+                        principalTable: "Posts",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_TaskID",
+                table: "Blogs",
+                column: "TaskID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
---
+++ Entity framework opgave/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("hejsa");
+    }
+}
+
+public class TodosContext : DbContext
+{
+    public DbSet<Todo> Blogs { get; set; }
+    public DbSet<Task> Posts { get; set; }
+
+    public string DbPath { get; }
+
+    public TodosContext()
+    {
+        //var folder = Environment.SpecialFolder.LocalApplicationData;
+        //var path = Environment.GetFolderPath(folder);
+        //DbPath = System.IO.Path.Join(path, "blogging.db");
+        DbPath = "C:/SqliteDbs/Test.db";
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Todo
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public bool Complete { get; set; }
+}
+
+public class Task
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    public List<Todo> Blog { get; set; }
+}
